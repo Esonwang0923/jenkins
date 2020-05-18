0a8c5217ce94bd4e2bb190877838f0397f6a4ef2
Administrator <15209828293@163.cm>
2020-05-18 17:04:20 +0800

    first

--- src/main/java/com/jenkins/test/jenkins/controller/MainController.java
+++ src/main/java/com/jenkins/test/jenkins/controller/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/api")
 public class MainController {
     @RequestMapping(value = "/test",method = RequestMethod.GET)
     public String hello() {
